8832143edbef54c5225e490d24050d092f89b7c7
zayennn <zaayeennn@gmail.com>
2025-10-17 09:01:07 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Protected Routes
 Route::middleware(['auth'])->group(function () {
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
